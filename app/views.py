0d5c3197da8855ead8f1835feaf34dec4a295c3d
KarenNgala <ngalalalu@gmail.com>
2020-07-04 22:34:27 +0300

    Add unit testing for sources and models file

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def index():
+def homepage():
     print(app.config)
     return render_template("index.html")
 
-@app.route('/sources/<name>')
-def sources(name):
-    return render_template("sources.html")
+@app.route('/articles/<name>')
+def articles(name):
+    return render_template("articles.html")
