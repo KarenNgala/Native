e61c3eae8858f02e0b80c5e57ad707ace8441afc
KarenNgala <ngalalalu@gmail.com>
2020-07-10 01:01:06 +0300

    404 error handling

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,4 @@ app.config.from_object(DevelopmentConfig)
 app.config.from_pyfile('config.py')
 
 from app import views
+from app import error
---
+++ app/error.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    '''
+    return render_template('error.html'), 404
